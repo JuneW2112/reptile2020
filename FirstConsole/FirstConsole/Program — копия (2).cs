df85566f48540db148097df5484b22f9e3a7ac2f
Irina <Irina@DESKTOP-EU29K64>
2020-12-16 23:49:45 +0300

    add

---
+++ FirstConsole/FirstConsole/Program — копия (2).cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        never give up
+    }
+}
